b628b7c5e223bc5368e4e4ba9b908b4bf2dddcf5
Mateus Soares <105178173+MateuzinhoX02@users.noreply.github.com>
2022-12-07 21:31:26 -0300

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -31,6 +31,7 @@ class ClientPrefs {
 	public static var imagesPersist:Bool = false;
 	public static var ghostTapping:Bool = true;
 	public static var hideTime:Bool = false;
+	public static var hitSounds:Bool = false;
 
 	//Every key has two binds, these binds are defined on defaultKeys! If you want your control to be changeable, you have to add it on ControlsSubState (inside OptionsState.hx)'s list
 	public static var keyBinds:Map<String, Dynamic> = new Map<String, Dynamic>();
@@ -85,6 +86,7 @@ class ClientPrefs {
 		FlxG.save.data.hideTime = hideTime;
 		FlxG.save.data.achievementsMap = Achievements.achievementsMap;
 		FlxG.save.data.henchmenDeath = Achievements.henchmenDeath;
+		FlxG.save.data.hitSounds = hitSounds;
 		FlxG.save.flush();
 
 		var save:FlxSave = new FlxSave();
@@ -97,6 +99,9 @@ class ClientPrefs {
 	public static function loadPrefs() {
 		if(FlxG.save.data.downScroll != null) {
 			downScroll = FlxG.save.data.downScroll;
+		}
+		if(FlxG.save.data.hitSounds != null) {
+			hitSounds = FlxG.save.data.hitSounds;
 		}
 		if(FlxG.save.data.optimizedMode != null) {
 			optimizedMode = FlxG.save.data.optimizedMode;
