b45e27d9028d89b33f65ebcd4a5e30be2c54f3f2
fang <95029829+Fangiolol@users.noreply.github.com>
2022-12-11 23:12:38 -0300

    Optimization

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -374,11 +374,6 @@ class PlayState extends MusicBeatState
 				var bg:BGSprite = new BGSprite('stageback', -600, -200, 0.9, 0.9);
 				add(bg);
 
-				var stageFront:BGSprite = new BGSprite('stagefront', -650, 600, 0.9, 0.9);
-				stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
-				stageFront.updateHitbox();
-				add(stageFront);
-
 				if(!ClientPrefs.lowQuality) {
 					var stageLight:BGSprite = new BGSprite('stage_light', -125, -100, 0.9, 0.9);
 					stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
