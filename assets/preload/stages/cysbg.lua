bae28620b98244bdb6222946aa689d2c6f94100b
fang <95029829+Fangiolol@users.noreply.github.com>
2022-12-19 09:17:27 -0300

    XPos fix

--- assets/preload/stages/cysbg.lua
+++ assets/preload/stages/cysbg.lua
@@ -1,9 +1,6 @@
 function onCreate()
-	-- background shit
-	makeLuaSprite('cysBG', 'cysBG', -500, -300);
+	makeLuaSprite('cysBG', 'cysBG', -500, -220);
 	setLuaSpriteScrollFactor('cysBG', 0.9, 0.9);
-	
 	addLuaSprite('cysBG', false);
-	
-	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+	close(true);
 end
