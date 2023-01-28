2c232ead740cb5f54fd506df40e7ba54791fb08a
fang <95029829+Fangiolol@users.noreply.github.com>
2023-01-28 18:30:37 -0300

    Update script.lua

--- assets/preload/data/test-stage/script.lua
+++ assets/preload/data/test-stage/script.lua
@@ -1,9 +1,9 @@
 function onCreate()
-if (songName == 'test-stage') then
-addLuaScript('data/scripts/dialogue')
-addLuaScript('data/scripts/smoothcam')
-addLuaScript('data/scripts/healthdrain')
---addLuaScript('data/scripts/playCutscene')
+if (songName == 'Test-Stage') then
+addLuaScript('scripts/dialogue')
+addLuaScript('scripts/smoothcam')
+addLuaScript('scripts/healthdrain')
+--addLuaScript('scripts/playCutscene')
 --only use cutscene if you have a mp4 vídeo on videos folder
 end
 end
