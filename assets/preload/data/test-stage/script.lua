eeefe82e8b308a703d6c724c56e362234f958fd1
fang <95029829+Fangiolol@users.noreply.github.com>
2023-01-28 21:47:52 -0300

    Add files via upload

--- assets/preload/data/test-stage/script.lua
+++ assets/preload/data/test-stage/script.lua
@@ -1,9 +1,7 @@
 function onCreate()
-if (songName == 'Test-Stage') then
 addLuaScript('scripts/dialogue')
 addLuaScript('scripts/smoothcam')
 addLuaScript('scripts/healthdrain')
 --addLuaScript('scripts/playCutscene')
 --only use cutscene if you have a mp4 vídeo on videos folder
-end
 end
