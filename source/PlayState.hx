716c82fc504f6005457cf74b878c7434d40bba3b
fang <95029829+Fangiolol@users.noreply.github.com>
2023-02-02 18:01:52 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1140,7 +1140,7 @@ class PlayState extends MusicBeatState
 		#if LUA_ALLOWED
 		var doPush:Bool = false;
 		var luaFile:String = 'characters/' + name + '.lua';
-		if(FileSystem.exists(Paths.getPreloadPath(luaToLoad)) {
+		if(FileSystem.exists(Paths.getPreloadPath(luaToLoad))) {
 			luaFile = Paths.mod(luaFile);
 			doPush = true;
 		} else {
