79dc270d7664698210f8e2961d6c94866e6bbbc8
Random people <98900217+justaguy6@users.noreply.github.com>
2023-02-06 20:52:27 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -266,7 +266,7 @@ class PlayState extends MusicBeatState
 	override public function create()
 	{
 		
-		Main.dumpCache();
+		
 		
         #if MODS_ALLOWED
  		Paths.destroyLoadedImages(resetSpriteCache);
