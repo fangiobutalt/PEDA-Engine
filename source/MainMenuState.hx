13a85ba8b55eb7e05730e448ebc1d8dc3f9eee68
fang <95029829+Fangiolol@users.noreply.github.com>
2023-09-24 02:15:43 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '0.4.2'; //This is also used for Discord RPC
-	public static var pedaEngineVersion:String = '0.5.1 (inDev Build)';
+	public static var pedaEngineVersion:String = '0.6.2-stable';
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
